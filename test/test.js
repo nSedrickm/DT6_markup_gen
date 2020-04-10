538396a705e0b6b9144abdc288c7d74adfde5742
nSedrickm <nsedrick101@gmail.com>
2020-04-10 21:06:37 +0100

    tests

---
+++ test/test.js
@@ -0,0 +1,12 @@
+// test.js
+import {gen_code , display} from '../js/code_generator';
+
+if(
+gen_code(h1, main_header, header_class, welcome) == '<h1 id="main_header" class="header_class">Welcome</h1>') {
+
+ console.log("function: gen_code test pass")
+}
+else {
+    console.log("function: gen_code: test fail");
+}
+
