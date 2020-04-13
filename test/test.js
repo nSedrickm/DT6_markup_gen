5976db5cc2e5656c863255d02e44f1a9e30e1de2
nsedrickm <nsedrick101@gmail.com>
2020-04-13 18:41:12 +0100

    added tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,32 @@
-// module test.js
-import {gen_code , display} from '../js/code_generator';
+//test.js
+//Loads after generator.js so function calls can be direct :)
+console.log("test.js loaded");
 
-if(
-gen_code(h1, main_header, header_class, welcome) == '<h1 id="main_header" class="header_class">Welcome</h1>') {
-
- console.log("function: gen_code test pass")
+//test get_values()
+get_values();
+if (tag) {
+  console.log("function: get_values() -> test:pass");
+} else {
+  console.log("function: get_values() -> test:fail");
 }
-else {
-    console.log("function: gen_code: test fail");
+
+//test gen_code()
+var test_tag = "h4",
+  test_id = "heading-4",
+  test_class = "h-class",
+  message = "Testing";
+if (
+  gen_code(test_tag, test_id, test_class, message) ==
+  "<h4 id='heading-4' class='h-class'>Testing</h4>"
+) {
+  console.log("function: gen_code() -> test:pass");
+} else {
+  console.log("function: gen_code() -> test:fail");
 }
 
+//test display()
+if (get_values() && gen_code(test_tag, test_id, test_class, message)) {
+  console.log("function: display() -> test:pass");
+} else {
+  console.log("function: display() -> test:fail");
+}
